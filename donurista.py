fa017a75ce82a61323ea8f203fd80510f5e07267
Alexander Gorbunov <s-kozelsk@yandex.ru>
2020-11-05 19:45:24 +0300

    Tho engines
    Two engines are working together now!

--- donurista.py
+++ donurista.py
@@ -3,6 +3,7 @@ import chess
 import chess.engine
 import logging
 from pyfiglet import Figlet
+from math import inf
 
 
 class Times:
@@ -25,6 +26,21 @@ class Times:
 		return L
 
 
+class Info:
+	def __init__(self, kwargs):
+		self.kwargs = kwargs
+
+	def __getattr__(self, attr):
+		return self.kwargs.get(attr)
+
+	def get_weight(self):
+		score = self.score
+		if score.is_mate():
+			return inf
+		score = int(str(score.relative))
+		return score
+
+
 class Donurista:
 	def __init__(self, brpath, nnpath, bropt, nnopt):
 		self.hello()
@@ -36,9 +52,6 @@ class Donurista:
 		try:
 			self.start()
 		except KeyboardInterrupt:
-			self.quit()
-		except Exception as exp:
-			print(exp)
 			self.quit()
 
 	def start(self):
@@ -74,6 +87,8 @@ class Donurista:
 		self.nn_options = dict()
 		self.br = chess.engine.SimpleEngine.popen_uci(self.brpath)
 		self.nn = chess.engine.SimpleEngine.popen_uci(self.nnpath)
+		self.prev_br = 0
+		self.prev_nn = 0
 		self.func_book = {
 			'uci': self.uci,
 			'isready': self.isready,
@@ -92,29 +107,39 @@ class Donurista:
 
 	def go(self, *args):
 		T = Times(*args)
-		L = T.get_limit()
-		move = self.best_move(self.br, L)
-		info = self.analyse(self.br, L)
+		L = T.get_limit(3)
+		info_br = self.analyse(self.br, L)
+		info_nn = self.analyse(self.nn, L)
+		score_br = info_br.get_weight()
+		score_nn = info_nn.get_weight()
+		dlt_br = score_br - self.prev_br
+		dlt_nn = score_nn - self.prev_nn
+		logging.info('[info br] ' + str(info_br.score))
+		logging.info('[info nn] ' + str(info_nn.score))
+		if dlt_br >= dlt_nn:
+			move = self.best_move(self.br, L)
+			self.show_info(info_br)
+			logging.info('[go br]')
+		else:
+			move = self.best_move(self.nn, L)
+			self.show_info(info_nn)
+			logging.info('[go nn]')
+		self.prev_br = score_br
+		self.prev_nn = score_nn	
 		self.board.push(move.move)
-		self.show_info(info)
-		logging.info('[info] ' + str(info))
 		print('bestmove {}'.format(move.move))
 
 	def show_info(self, info):
-		depth = info['depth']
-		seldepth = info['seldepth']
-		multipv = info['multipv']
-		score = 'cp ' + str(info['score'].relative) if not info['score'].is_mate() else 'mate ' + str(info['score'].relative)[1:]
-		nodes = info['nodes']
-		nps = info['nps']
-		tbhits = info['tbhits']
-		time = int(info['time'] * 1e3)
+		depth, seldepth, multipv = info.depth, info.seldepth, info.multipv
+		score = 'cp ' + str(info.score.relative) if not info.score.is_mate() else 'mate ' + str(info.score.relative)[1:]
+		nodes, nps, tbhits = info.nodes, info.nps, info.tbhits
+		time = int(info.time * 1e3)
 		print('info depth {} seldepth {} multipv {} score {} nodes {} nps {} tbhits {} time {}'.format(
 			depth, seldepth, multipv, score, nodes, nps, tbhits, time))
 
 	def analyse(self, eng, L):
 		info = eng.analyse(self.board, L)
-		return info
+		return Info(info)
 
 	def best_move(self, eng, L):
 		move = eng.play(self.board, L)
