996802881cf10d91df42444f2c76df447054c44e
Alexander Gorbunov <s-kozelsk@yandex.ru>
2020-11-13 12:26:51 +0300

    Leran commit II
    16802 positions are in the db now! Played with Stockfish 12 after opening and before

--- db-viewer.py
+++ db-viewer.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 from unqlite import UnQLite
 from prettytable import PrettyTable
+from argparse import ArgumentParser
 
 
-P = PrettyTable(['#', 'Fen', 'Depth', 'Move'])
+def print_all():
+	P = PrettyTable(['#', 'Fen', 'Depth', 'Move'])
+	for i, (fen, info) in enumerate(db):
+		info = info.decode('utf-8')
+		depth, move = info.split(';')
+		P.add_row([i + 1, fen, depth, move])
+	print(P)
+
+
+parser = ArgumentParser(description='Donurista DB viewer')
+parser.add_argument('--all', '-a', action='store_const', help='print all positions (long time)', const=True, default=False)
 db = UnQLite('donurista.db')
-for i, (fen, info) in enumerate(db):
-	info = info.decode('utf-8')
-	depth, move = info.split(';')
-	P.add_row([i + 1, fen, depth, move])
+args = parser.parse_args()
+if args.all:
+	print_all()
+else:
+	print(len(db))
 db.close()
-print(P)
